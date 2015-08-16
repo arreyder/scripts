9e015ce03701b49c3f759a1f0601f3d221df0791
arreyder <arreyder@arreyder.com>
2015-08-16 12:35:16 -0500

    quick hack to get cpu use by threads

---
+++ jstats_top.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+system("clear");
+my $ppid = `jps -lv | awk '/CassandraDaemon/ {print \$1}'`;
+
+while(1) {
+  my %h;
+  chomp $ppid;
+  my @jstack = `sudo -u cassandra jstack $ppid`;
+  my $cmd = "top -d0.1 -n1 -b -u cassandra -H | awk '/cassandr/ {print \$1,\$9}'";
+  my @threads = `$cmd`;
+  foreach (@threads) {
+    chomp;
+    (my $k,my $v) = split;
+    $h{$k}=$v;
+  }
+
+  my @keys = sort {
+    $h{$b} <=> $h{$a}
+    or
+    "\L$a" cmp "\L$b"
+  } keys %h;
+
+  my @highest = @keys[0..20];
+
+  foreach my $k (@highest) {
+    my $hex = sprintf("0x%x", $k);
+    foreach (@jstack) {
+      chomp;
+      print "\%CPU:$h{$k}, PID:$k THREAD:$_\n" if ($_  =~ m/$hex/);
+    }
+  }
+  sleep 5;
+  system("clear");
+}
+
