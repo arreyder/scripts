d070214efa90ce8e3aacd10105bef8d8336f24fa
arreyder <arreyder@arreyder.com>
2015-08-17 11:52:18 -0500

    take the pattern and user to run as via args

--- monitoring/jstats_top.pl
+++ monitoring/jstats_top.pl
@@ -2,14 +2,24 @@
 use strict;
 use warnings;
 
+my $num_args = $#ARGV + 1;
+if ($num_args != 2) {
+  print "\nUsage: jstat_top.pl <user the jvm runs as>  <process id as reported by jps>\n";
+  exit;
+}
+
+my $user = $ARGV[0];
+my $process_id = $ARGV[1];
+$process_id = substr( $process_id, 0, 7 );
+
 system("clear");
-my $ppid = `sudo -u cassandra jps -lv | awk '/CassandraDaemon/ {print \$1}'`;
+my $ppid = `sudo -u $user jps -lv | awk '/$process_id/ {print \$1}'`;
 chomp $ppid;
 
 while(1) {
   my %h;
-  my @jstack = `sudo -u cassandra jstack $ppid`;
-  my $cmd = "top -d0.1 -n1 -b -u cassandra -H | awk '/cassandr/ {print \$1,\$9}'";
+  my @jstack = `sudo -u $user jstack $ppid`;
+  my $cmd = "top -d0.1 -n1 -b -u $user -H | awk '/$process_id/ {print \$1,\$9}'";
   my @threads = `$cmd`;
   foreach (@threads) {
     chomp;
